67754cf835bad19276ee19749fafc44dd1d5d480
helloxhk <helloxhk@163.com>
2018-12-04 11:25:46 +0800

    异常 exception

--- src/com/xhk/web/servlet/container/Snow.java
+++ src/com/xhk/web/servlet/container/Snow.java
@@ -1,9 +1,6 @@
 package com.xhk.web.servlet.container;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author xhk
---
+++ src/com/xhk/web/servlet/exception/LoggingException.java
@@ -0,0 +1,33 @@
+package com.xhk.web.servlet.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+/**
+ * @author xhk
+ * @time 2018-12-03 18:07
+ */
+public class LoggingException extends Exception{
+
+	private static Logger logger = Logger.getLogger("LoggingException");
+
+	public LoggingException() {
+		StringWriter sw = new StringWriter();
+		printStackTrace(new PrintWriter(sw));
+		logger.severe(sw.toString());
+	}
+
+	public static void main(String[] args) {
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e) {
+			System.err.println("Caught " + e);
+		}
+		try {
+			throw new LoggingException();
+		} catch (LoggingException e) {
+			System.err.println("Caught " + e);
+		}
+	}
+}
