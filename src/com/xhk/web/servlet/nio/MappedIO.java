63325c3b7835425f1b6dc6d015584f4d637138b7
helloxhk <helloxhk@163.com>
2018-12-19 13:04:42 +0800

    压缩 compress

--- src/com/xhk/web/servlet/nio/MappedIO.java
+++ src/com/xhk/web/servlet/nio/MappedIO.java
@@ -2,7 +2,9 @@ package com.xhk.web.servlet.nio;
 
 import java.io.*;
 import java.nio.IntBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.RandomAccess;
 
 /**
  * @author xhk
@@ -26,8 +28,8 @@ public class MappedIO {
 				long start = System.nanoTime();
 				test();
 				long time = System.nanoTime() - start;
-				System.out.println(String.format("耗时：%s",time));
-			} catch (FileNotFoundException e) {
+				System.out.println(String.format("耗时：%s", time));
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
@@ -36,55 +38,70 @@ public class MappedIO {
 	}
 
 	private static Tester[] testers = {
-		new Tester("Stream Write") {
-			@Override
-			public void test() throws IOException {
-				DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("src/temp/io.temp")));
-				for (int i = 0; i < size; i++) {
-					dos.writeInt(i);
+			new Tester("Stream Write") {
+				@Override
+				public void test() throws IOException {
+					DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("src/temp/io.temp")));
+					for (int i = 0; i < size; i++) {
+						dos.writeInt(i);
+					}
+					dos.close();
 				}
-				dos.close();
-			}
-		},
-		new Tester("Mapped Write") {
-			@Override
-			public void test() throws IOException {
-				FileChannel fc = new RandomAccessFile("src/temp/io.temp", "rw").getChannel();
-				IntBuffer ib = fc.map(FileChannel.MapMode.READ_WRITE, 0, fc.size()).asIntBuffer();
-				for (int i = 0; i < size; i++) {
-					ib.put(i);
+			},
+			new Tester("Mapped Write") {
+				@Override
+				public void test() throws IOException {
+					FileChannel fc = new RandomAccessFile("src/temp/io.temp", "rw").getChannel();
+					IntBuffer ib = fc.map(FileChannel.MapMode.READ_WRITE, 0, fc.size()).asIntBuffer();
+					for (int i = 0; i < size; i++) {
+						ib.put(i);
+					}
+					fc.close();
 				}
-				fc.close();
-			}
-		},
-		new Tester("Stream Read") {
-			@Override
-			public void test() throws IOException {
-				DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream("src/temp/io.temp")));
-				for (int i = 0; i < size; i++) {
-					dis.readInt();
+			},
+			new Tester("Stream Read") {
+				@Override
+				public void test() throws IOException {
+					DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream("src/temp/io.temp")));
+					for (int i = 0; i < dis.available(); i++) {
+						dis.readInt();
+					}
+					dis.close();
 				}
-				dis.close();
-			}
-		},
-		new Tester("Mapped Read") throws IOException{
-			@Override
-			public void test() {
-
-			}
-		},
-		new Tester("Stream Read/Write") throws IOException{
-			@Override
-			public void test() {
-
-			}
-		},
-		new Tester("Mapped Read/Write") throws IOException{
-			@Override
-			public void test() {
-
-			}
-		},
+			},
+			new Tester("Mapped Read") {
+				@Override
+				public void test() throws IOException {
+					IntBuffer ib = new RandomAccessFile("src/temp/io.temp", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, size).asIntBuffer();
+					while (ib.hasRemaining())
+						ib.get();
+				}
+			},
+			new Tester("Stream Read/Write") {
+				@Override
+				public void test() throws IOException {
+					RandomAccessFile raf = new RandomAccessFile("src/temp/io.temp", "rw");
+					raf.writeInt(1);
+					for (int i = 0; i < buffSize; i++) {
+						raf.seek(raf.length() - 4);
+						raf.writeInt(raf.readInt());
+					}
+				}
+			},
+			new Tester("Mapped Read/Write") {
+				@Override
+				public void test() throws IOException {
+					IntBuffer ib = new RandomAccessFile("src/temp/io.temp", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, size).asIntBuffer();
+					/**
+					 * java.io.FileNotFoundException: src\temp\io.temp (请求的操作无法在使用用户映射区域打开的文件上执行。)
+					 */
+//					IntBuffer ib = new FileOutputStream("src/temp/io.temp").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, size).asIntBuffer();
+					ib.put(1);
+					for (int i = 1; i < buffSize; i++) {
+						ib.put(ib.get(i - 1));
+					}
+				}
+			},
 	};
 
 	public static void main(String[] args) {
